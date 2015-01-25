cef867c8d88d4eaff99d58a5456fa3490fcbb143
Ryan <hiphubryan@gmail.com>
2015-01-25 17:06:38 +0800

    Finish class5

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Ryan on 2015/1/24.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Ryan on 2015/1/24.
+//  Copyright (c) 2015年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
